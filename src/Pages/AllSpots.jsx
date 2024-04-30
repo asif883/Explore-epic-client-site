d41ce5d2ffbdd553bd56f343e63e57a67377e53c
asif883 <asifiqbal95863@gmail.com>
2024-04-30 19:36:08 +0600

    sort

--- src/Pages/AllSpots.jsx
+++ src/Pages/AllSpots.jsx
@@ -1,15 +1,31 @@
 import { Link, useLoaderData } from "react-router-dom";
 import { IoLocationSharp } from "react-icons/io5";
+import { IoIosArrowDown } from "react-icons/io";
+
+
 
 
 const AllSpots = () => {
+
+
     const loadedData = useLoaderData()
     console.log(loadedData);
+
 
     // spotName,country_name,image,location,average_cost,seasonality, travel_time,total_visitors_per_year,short_description
   
     return (
         <div>
+          <div className="text-center mt-6 mb-10">
+          <div className="dropdown">
+              <div tabIndex={0} role="button" className="border-2 px-4  rounded-lg py-3 bg-[#FF9933] text-white text-lg font-semibold"><span className="flex gap-3 items-center">Sort <IoIosArrowDown /></span></div>
+              <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 text-[#FF9933] font-semibold rounded-box w-52">
+                <li><a>All</a></li>
+                <li><a>Cost low to high</a></li>
+                <li><a>Cost high to low</a></li>
+              </ul>
+          </div>
+          </div>
           <div className="grid gap-6 grid-cols-1 md:grid-cols-2 ">
             {
                 loadedData?.map((data,idx) => <div key={idx}>
@@ -36,24 +52,7 @@ const AllSpots = () => {
                     </div>
                   </div>
 
-                 {/* <div className="card  bg-base-100 shadow-xl">
-                        <figure className="px-10 pt-10">
-                            <img src={data.image} alt="Shoes" className="w-full h-[200px] rounded-xl" />
-                        </figure>
-                        <div className="card-body items-center text-center">
-                            <h2 className="card-title">{data.spotName}</h2>
-                            <p>{data.location} , {data.country_name}</p>
-                            <div className="flex gap-3">
-                            <p>{data.average_cost}</p>
-                            <p>{data.total_visitors_per_year}</p>
-                            <p>{data.seasonality}</p>
-                            </div>
-                            <div className="card-actions">
-                            <Link to={`details/${data._id}`}><button className="border-2 mt-4  px-4  rounded-lg py-3 bg-[#FF9933] text-white  font-semibold">View Details</button>
-                            </Link>
-                            </div>
-                        </div>
-                      </div> */}
+               
                 </div>)
             }
           </div>
